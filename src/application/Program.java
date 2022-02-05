6387e7f987ccd4c845b539b5aab673a2404e55e7
Paulo Rodrigo <paulorodrigo1992@gmail.com>
2022-02-05 17:38:21 -0300

    Recuperando Dados

---
+++ src/application/Program.java
@@ -0,0 +1,42 @@
+package application;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Connection conn = null; //preparar o bancos
+        Statement st = null; // criar a sql
+        ResultSet rs = null; // obter o resultado
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from department");
+
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+
+
+
+
+    }
+}
